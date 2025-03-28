7c300be494d84c67a04ff244a83b64d06a32b1a9
Lucas-Guercheski <168201998+Lucas-Tigre@users.noreply.github.com>
2025-03-28 10:54:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const randomNumber = Math.floor(Math.random() * 20) + 1;
+let attempts = 0;
 
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById('guess').value);
+    attempts++;
+
+    if (userGuess < 1 || userGuess > 20) {
+        document.getElementById('result').innerText = "Por favor, escolha um número entre 1 e 20.";
+    } else if (userGuess < randomNumber) {
+        document.getElementById('result').innerText = "Muito baixo! Tente novamente.";
+    } else if (userGuess > randomNumber) {
+        document.getElementById('result').innerText = "Muito alto! Tente novamente.";
+    } else {
+        document.getElementById('result').innerText = `Parabéns! Você acertou o pato! 🦆 O número era ${randomNumber} e você acertou em ${attempts} tentativas!`;
+    }
+}
